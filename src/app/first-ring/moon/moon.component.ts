60b6b71249f093261f5487aadfa119bba4629003
Eva <evavanderscheer@live.nl>
2023-02-09 22:58:04 +0100

    Cheesy moon

--- src/app/first-ring/moon/moon.component.ts
+++ src/app/first-ring/moon/moon.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-moon',
   templateUrl: './moon.component.html',
   styleUrls: ['./moon.component.scss']
 })
-export class MoonComponent implements OnInit {
-
+export class MoonComponent implements OnInit, OnDestroy {
+  images : HTMLImageElement[] = [];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  placeFlag() {
+    const img = document.createElement("img");
+    img.src = '../../../assets/images/cheese-flag.png';
+    img.height = 100;
+    img.alt = 'flag';
+
+    // set the position
+    img.style.position = 'absolute';
+    img.style.top = (document.body.clientHeight - 200) * Math.random() + 100 + 'px';
+    img.style.left = (document.body.clientWidth - 100) * Math.random() + 'px';
+    console.log(img.style.top);
+    this.images.push(img);
+
+    document.body.appendChild(img);
+  }
+
+  ngOnDestroy(): void {
+    for(let img of this.images){
+      document.body.removeChild(img);
+    }
   }
 
 }
