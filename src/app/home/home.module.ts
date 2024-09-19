1d8c49463538bd16990737a5c1e86fe5c55daf3f
evdscheer <evdscheer@quintor.nl>
2024-09-19 17:56:56 +0200

    Try fix language path

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -11,8 +11,8 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-export function HttpLoaderFactory(http:HttpClient){
-  return new TranslateHttpLoader(http)
+export function HttpLoaderFactory(http: HttpClient) {
+	return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
 }
 
 @NgModule({
