01b585cf2b8709b48b5b6e144a8face0252fc796
evdscheer <evdscheer@quintor.nl>
2024-09-19 16:45:46 +0200

    switch languages

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  private currentLang: string;
+
+  constructor(private translateService: TranslateService) {
+    translateService.setDefaultLang('en');
+    translateService.use(localStorage.getItem('lang') || 'en')
+    this.currentLang = localStorage.getItem('language') || 'en';
+  }
+
   ngOnInit(): void {
   }
-  
+
+  getLanguage(): string {
+    return this.currentLang;
+  }
+
+  toggleLanguage(): void {
+    this.currentLang = this.currentLang === 'en' ? 'nl' : 'en';
+    localStorage.setItem('language', this.currentLang);
+    this.translateService.use(this.currentLang);
+  }
+
 }
